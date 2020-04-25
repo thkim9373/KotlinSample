e503f8d5fa751b99b8299625d6db712a27f7fc3d
TaeHoon <thkim9373@gmail.com>
2020-04-25 23:55:43 +0900

    Contact example save commit.

--- app/src/main/java/com/hoony/kotlinsample/content_provider/contact/ContactViewModel.kt
+++ app/src/main/java/com/hoony/kotlinsample/content_provider/contact/ContactViewModel.kt
@@ -1,10 +1,26 @@
 package com.hoony.kotlinsample.content_provider.contact
 
 import android.app.Application
-import android.content.ContentProvider
-import android.content.ContentResolver
+import android.database.Cursor
+import android.provider.ContactsContract
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.hoony.kotlinsample.content_provider.data.Contact
 
-class ContactViewModel : AndroidViewModel(Application()) {
+class ContactViewModel(application: Application) : AndroidViewModel(application) {
+    private val contactCursor: Cursor? by lazy {
+        application.contentResolver.query(
+            ContactsContract.Contacts.CONTENT_URI,
+            null,
+            null,
+            null,
+            null
+        )
+    }
 
+    val contactListLiveData: LiveData<List<Contact>> by lazy {
+        contactCursor?.let {
+
+        }
+    }
 }
---
+++ app/src/main/java/com/hoony/kotlinsample/content_provider/data/Contact.kt
@@ -0,0 +1,6 @@
+package com.hoony.kotlinsample.content_provider.data
+
+data class Contact(
+    val name: String,
+    val phoneNumber: String
+)
