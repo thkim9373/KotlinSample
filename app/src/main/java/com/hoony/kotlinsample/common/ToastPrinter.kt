eb04bf9b8e65d3265a546903a4cc922e25916521
thkim9373 <thkim9373@gmail.com>
2020-05-06 00:16:50 +0900

    a

--- app/src/main/java/com/hoony/kotlinsample/common/ToastPrinter.kt
+++ app/src/main/java/com/hoony/kotlinsample/common/ToastPrinter.kt
@@ -8,7 +8,6 @@ class ToastPrinter {
         private var toast: Toast? = null
         fun showToast(context: Context, msg: String) {
             toast?.cancel()
-
             toast = Toast.makeText(context, msg, Toast.LENGTH_SHORT)
             toast?.show()
         }
