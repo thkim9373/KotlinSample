4fd337aab1e225576146ec3d0d551bc9695c5fd1
TaeHoon <thkim9373@gmail.com>
2020-05-07 23:55:18 +0900

    Video example update.

--- app/src/main/java/com/hoony/kotlinsample/content_provider/video/player/PlayerFragment.kt
+++ app/src/main/java/com/hoony/kotlinsample/content_provider/video/player/PlayerFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -22,6 +23,29 @@ class PlayerFragment : Fragment() {
     private lateinit var binding: FragmentPlayerBinding
     private lateinit var viewModel: VideoViewModel
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(
+            viewModelStore,
+            ViewModelProvider.AndroidViewModelFactory(requireActivity().application)
+        ).get(VideoViewModel::class.java)
+
+        setObserver()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+
+            }
+        }
+
+        requireActivity().onBackPressedDispatcher.addCallback(this, callback)
+    }
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -36,16 +60,10 @@ class PlayerFragment : Fragment() {
         return binding.root
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        viewModel = ViewModelProvider(
-            viewModelStore,
-            ViewModelProvider.AndroidViewModelFactory(requireActivity().application)
-        ).get(VideoViewModel::class.java)
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        
         mPlayer = ExoPlayerFactory.newSimpleInstance(requireContext())
-
-        setObserver()
     }
 
     private fun setObserver() {
