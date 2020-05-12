7036e1e7f3e617ed7475990022eb436e0b913ec9
TaeHoon <thkim9373@gmail.com>
2020-05-12 23:48:22 +0900

    Video example update.

--- app/src/main/java/com/hoony/kotlinsample/content_provider/audio/AudioViewModel.kt
+++ app/src/main/java/com/hoony/kotlinsample/content_provider/audio/AudioViewModel.kt
@@ -1,6 +1,7 @@
 package com.hoony.kotlinsample.content_provider.audio
 
 import android.app.Application
+import android.content.ContentUris
 import android.database.Cursor
 import android.net.Uri
 import android.provider.MediaStore
@@ -8,7 +9,6 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.liveData
 import com.hoony.kotlinsample.content_provider.data.Album
-import java.io.File
 
 class AudioViewModel(application: Application) : AndroidViewModel(application) {
     private val audioInfoCursor: Cursor? by lazy {
@@ -26,18 +26,18 @@ class AudioViewModel(application: Application) : AndroidViewModel(application) {
         val albumList = ArrayList<Album>()
 
         audioInfoCursor?.let {
-            val albumArtIndex = it.getColumnIndexOrThrow(MediaStore.Audio.Albums.ALBUM_ART)
+            val idIndex = it.getColumnIndexOrThrow(MediaStore.Audio.Albums._ID)
             val artistIndex = it.getColumnIndexOrThrow(MediaStore.Audio.Albums.ARTIST)
             val numOfSongIndex = it.getColumnIndexOrThrow(MediaStore.Audio.Albums.NUMBER_OF_SONGS)
+
 
             while (it.moveToNext()) {
                 albumList.add(
                     Album(
-                        if (it.getString(albumArtIndex) != null) {
-                            Uri.fromFile(File(it.getString(albumArtIndex)))
-                        } else {
-                            null
-                        },
+                        ContentUris.withAppendedId(
+                            Uri.parse("content://media/external/audio/albumart"),
+                            it.getLong(idIndex)
+                        ),
                         it.getString(artistIndex),
                         it.getInt(numOfSongIndex)
                     )
