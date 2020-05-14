934a4d1af27eccaac85d0baa4c84f1d7318a7e3a
TaeHoon <thkim9373@gmail.com>
2020-05-14 23:16:33 +0900

    Custom view example first commit.

---
+++ app/src/main/java/com/hoony/kotlinsample/custom_view/CustomViewActivity.kt
@@ -0,0 +1,6 @@
+package com.hoony.kotlinsample.custom_view
+
+import androidx.appcompat.app.AppCompatActivity
+
+class CustomViewActivity : AppCompatActivity() {
+}
---
+++ app/src/main/java/com/hoony/kotlinsample/custom_view/EmotionalFaceView.kt
@@ -0,0 +1,8 @@
+package com.hoony.kotlinsample.custom_view
+
+import android.content.Context
+import android.view.View
+
+class EmotionalFaceView(context: Context) : View(context) {
+
+}
