2d2d26b7f86fcb8742f9ef73ed07fe79334d7c86
TaeHoon <thkim9373@gmail.com>
2020-05-21 23:57:15 +0900

    Saved state view model first commit.

--- app/src/main/java/com/hoony/kotlinsample/saved_state_view_model/SavedStateViewModelActivity.kt
+++ app/src/main/java/com/hoony/kotlinsample/saved_state_view_model/SavedStateViewModelActivity.kt
@@ -1,6 +1,31 @@
 package com.hoony.kotlinsample.saved_state_view_model
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.hoony.kotlinsample.R
+import com.hoony.kotlinsample.databinding.ActivitySavedStateViewModelBinding
 
 class SavedStateViewModelActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySavedStateViewModelBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_saved_state_view_model)
+
+        savedInstanceState?.let {
+            val count = it.getInt("count")
+            binding.tvCount.text = count.toString()
+        }
+
+        binding.fabAdd.setOnClickListener {
+            var count = Integer.parseInt(binding.tvCount.text.toString())
+            binding.tvCount.text = (++count).toString()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt("count", Integer.parseInt(binding.tvCount.text.toString()))
+        super.onSaveInstanceState(outState)
+    }
 }
