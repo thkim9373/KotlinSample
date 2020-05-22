f1f4cfd418377699990156e329e2e02389ece24b
TaeHoon <thkim9373@gmail.com>
2020-05-22 23:20:54 +0900

    Saved state view model example update.

--- app/src/main/java/com/hoony/kotlinsample/saved_state_view_model/SavedStateViewModel.kt
+++ app/src/main/java/com/hoony/kotlinsample/saved_state_view_model/SavedStateViewModel.kt
@@ -1,8 +1,28 @@
 package com.hoony.kotlinsample.saved_state_view_model
 
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 
-class SavedStateViewModel(application: Application) : AndroidViewModel(application) {
-    
+class SavedStateViewModel(private val handle: SavedStateHandle) : ViewModel() {
+
+    // Get value of SavedStateHandle
+    private var counter = handle.get<Int>("counter") ?: 0
+        set(value) {
+            // Set value of SavedStateHandle
+            handle.set("counter", value)
+            field = value
+        }
+
+    private val _countForm = MutableLiveData<Int>(counter)
+    val countState: LiveData<Int> = _countForm
+
+//    // Get LiveData of SavedStateHandle
+//    val countLiveData: LiveData<Int> = handle.getLiveData("count", 0)
+
+    fun incCounter() {
+        ++counter
+        _countForm.value = counter
+    }
 }
--- app/src/main/java/com/hoony/kotlinsample/saved_state_view_model/SavedStateViewModelActivity.kt
+++ app/src/main/java/com/hoony/kotlinsample/saved_state_view_model/SavedStateViewModelActivity.kt
@@ -3,29 +3,55 @@ package com.hoony.kotlinsample.saved_state_view_model
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.SavedStateViewModelFactory
+import androidx.lifecycle.ViewModelProvider
 import com.hoony.kotlinsample.R
 import com.hoony.kotlinsample.databinding.ActivitySavedStateViewModelBinding
 
+// This example from : http://pluu.github.io/blog/android/2020/02/10/saved-state/
 class SavedStateViewModelActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySavedStateViewModelBinding
+    private lateinit var viewModel: SavedStateViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_saved_state_view_model)
+        viewModel = ViewModelProvider(
+            this,
+            SavedStateViewModelFactory(application, this)
+        ).get(SavedStateViewModel::class.java)
 
-        savedInstanceState?.let {
-            val count = it.getInt("count")
-            binding.tvCount.text = count.toString()
-        }
+//        savedInstanceState?.let {
+//            val count = it.getInt("count")
+//            binding.tvCount.text = count.toString()
+//        }
 
+        setObserver()
+        setListener()
+    }
+
+    private fun setObserver() {
+        viewModel.countState.observe(
+            this,
+            Observer {
+                binding.tvCount.text = it.toString()
+            }
+        )
+    }
+
+    private fun setListener() {
+//        binding.fabAdd.setOnClickListener {
+//            var count = Integer.parseInt(binding.tvCount.text.toString())
+//            binding.tvCount.text = (++count).toString()
+//        }
         binding.fabAdd.setOnClickListener {
-            var count = Integer.parseInt(binding.tvCount.text.toString())
-            binding.tvCount.text = (++count).toString()
+            viewModel.incCounter()
         }
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        outState.putInt("count", Integer.parseInt(binding.tvCount.text.toString()))
-        super.onSaveInstanceState(outState)
-    }
+//    override fun onSaveInstanceState(outState: Bundle) {
+//        outState.putInt("count", Integer.parseInt(binding.tvCount.text.toString()))
+//        super.onSaveInstanceState(outState)
+//    }
 }
