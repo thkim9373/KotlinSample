fc30056b5af4869d1e8e51d5fe8300cea491c781
thkim9373 <thkim9373@gmail.com>
2020-05-27 18:56:13 +0900

    Rx java example update.

--- app/src/main/java/com/hoony/kotlinsample/rx_java/example1/RxJavaExample1Activity.kt
+++ app/src/main/java/com/hoony/kotlinsample/rx_java/example1/RxJavaExample1Activity.kt
@@ -1,5 +1,6 @@
 package com.hoony.kotlinsample.rx_java.example1
 
+
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -10,31 +11,69 @@ import rx.Subscriber
 
 class RxJavaExample1Activity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityRxJavaExample1Binding
+    private lateinit var binding: ActivityRxJavaExample1Binding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_rx_java_example_1)
 
+        exportHelloWorld1()
+        exportHelloWorld2()
+        exportHelloWorldWithNameTag()
+        exportHelloWorldWithMapOnMap()
+    }
+
+    private fun exportHelloWorld1() {
         val observable = Observable.create<String> {
-            it.onNext("Hello world!")
+            it.onNext("Example1 : Hello world!")
             it.onCompleted()
         }
 
         val subscriber = object : Subscriber<String>() {
             override fun onNext(t: String?) {
-                binding.tv.text = t
+                binding.tv1.text = t
             }
 
             override fun onCompleted() {
-                TODO("Not yet implemented")
             }
 
             override fun onError(e: Throwable?) {
-                TODO("Not yet implemented")
             }
         }
 
         observable.subscribe(subscriber)
+    }
+
+    private fun exportHelloWorld2() {
+        Observable.just("Example2 : Hello world!!!")
+            .subscribe {
+                binding.tv2.text = it
+            }
+    }
+
+    private fun exportHelloWorldWithNameTag() {
+        Observable.just("Example3 : Hello world!")
+            .map {
+                "$it -isaac"
+            }
+            .subscribe {
+                binding.tv3.text = it
+            }
+    }
+
+    private fun exportHelloWorldWithMapOnMap() {
+        Observable.just("Example4 : Hello world!!!")
+            .map {
+                "$it -isaac"
+            }
+            .map {
+                it.hashCode()
+            }
+            .map {
+                it.toString()
+            }
+            .subscribe {
+                binding.tv4.text = it
+            }
     }
 }
