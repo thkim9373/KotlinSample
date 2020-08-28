476943af07a8f0e58a504b13c6cdb4a0f5268a48
TaeHoon <thkim9373@gmail.com>
2020-08-28 23:27:08 +0900

    Save commit.

--- app/src/main/java/com/hoony/kotlinsample/notification/NotificationActivity.kt
+++ app/src/main/java/com/hoony/kotlinsample/notification/NotificationActivity.kt
@@ -1,7 +1,13 @@
 package com.hoony.kotlinsample.notification
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.hoony.kotlinsample.R
 
 class NotificationActivity : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_list)
+    }
 }
