500536b4336d51f2639535a1aa6890c00f7670dc
TaeHoon <thkim9373@gmail.com>
2020-08-31 22:46:48 +0900

    Save commit.

---
+++ app/src/main/java/com/hoony/kotlinsample/notification/BigTextFragment.kt
@@ -0,0 +1,40 @@
+package com.hoony.kotlinsample.notification
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.hoony.kotlinsample.R
+import com.hoony.kotlinsample.databinding.FragmentBigTextBinding
+
+class BigTextFragment : Fragment() {
+
+    private lateinit var binding: FragmentBigTextBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_big_text, container, false)
+        binding = DataBindingUtil.bind(view)!!
+        return view
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.bigText.setText(
+            "살어리 살어리랏다" +
+                    "청산에 살어리랏다" +
+                    "멀위랑 다래랑 먹고" +
+                    "청산에 살어리랏다" +
+                    "얄리얄리얄라셩얄라리얄라"
+        )
+    }
+
+    fun getBigText(): String = binding.bigText.editableText.toString()
+}
--- app/src/main/java/com/hoony/kotlinsample/notification/NotificationActivity.kt
+++ app/src/main/java/com/hoony/kotlinsample/notification/NotificationActivity.kt
@@ -3,7 +3,9 @@ package com.hoony.kotlinsample.notification
 import android.app.NotificationManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.hoony.kotlinsample.R
 import com.hoony.kotlinsample.databinding.ActivityNotificationBinding
 import kotlinx.android.synthetic.main.activity_notification.*
@@ -11,6 +13,9 @@ import kotlinx.android.synthetic.main.activity_notification.*
 class NotificationActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityNotificationBinding
+
+    private var nowFragmentName: String = ""
+    private val bigTextFragment: BigTextFragment = BigTextFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,26 +35,41 @@ class NotificationActivity : AppCompatActivity() {
             )
         }
         binding.styleGroup.setOnCheckedChangeListener { _, checkedId ->
-            when (checkedId) {
-                R.id.defaultStyle -> {
-
-                }
-                R.id.bigTextStyle -> {
-
-                }
-                R.id.bigPictureStyle -> {
-
-                }
-                R.id.messagingStyle -> {
-
-                }
-                R.id.mediaStyle -> {
-
-                }
-                R.id.inboxStyle -> {
-
+            val fragment = getFragment(checkedId)
+            if (fragment != null) {
+                nowFragmentName = fragment.toString()
+                supportFragmentManager.beginTransaction()
+                    .replace(binding.customItemLayout.id, fragment, nowFragmentName).commit()
+            } else {
+                val nowFragment = supportFragmentManager.findFragmentByTag(nowFragmentName)
+                if (nowFragment != null && nowFragment.isVisible) {
+                    supportFragmentManager.beginTransaction().remove(nowFragment).commit()
                 }
             }
         }
     }
+
+    private fun getFragment(checkedId: Int): Fragment? =
+        when (checkedId) {
+            R.id.defaultStyle -> null
+            R.id.bigTextStyle -> bigTextFragment
+            R.id.bigPictureStyle -> bigTextFragment
+            R.id.messagingStyle -> bigTextFragment
+            R.id.mediaStyle -> bigTextFragment
+            R.id.inboxStyle -> bigTextFragment
+            else -> null
+        }
+
+    private fun getNotificationStyle(checkedId: Int): NotificationCompat.Style? =
+        when (checkedId) {
+            R.id.defaultStyle -> null
+            R.id.bigTextStyle -> NotificationCompat.BigTextStyle()
+            R.id.bigPictureStyle -> NotificationCompat.BigPictureStyle()
+            R.id.messagingStyle -> {
+                NotificationCompat.MessagingStyle("User")
+            }
+            R.id.mediaStyle -> androidx.media.app.NotificationCompat.MediaStyle()
+            R.id.inboxStyle -> NotificationCompat.InboxStyle()
+            else -> null
+        }
 }
