8f222419b43cc6ce43458208284c61064e36f913
thkim9373 <thkim9373@gmail.com>
2020-10-16 23:43:08 +0900

    save commit

--- app/src/main/java/com/hoony/kotlinsample/toast/ToastActivity.kt
+++ app/src/main/java/com/hoony/kotlinsample/toast/ToastActivity.kt
@@ -1,7 +1,17 @@
 package com.hoony.kotlinsample.toast
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.hoony.kotlinsample.databinding.ActivityToastBinding
 
 class ToastActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityToastBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityToastBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
 }
+
