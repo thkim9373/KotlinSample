34d91e09d4fb89aaadc17bdf13a24304f570db30
thkim9373 <thkim9373@gmail.com>
2020-10-26 19:34:32 +0900

    save commit

--- app/src/main/java/com/hoony/kotlinsample/kakao/message/MessageActivity.kt
+++ app/src/main/java/com/hoony/kotlinsample/kakao/message/MessageActivity.kt
@@ -1,7 +1,19 @@
 package com.hoony.kotlinsample.kakao.message
 
+import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.hoony.kotlinsample.databinding.ActivityKokoaMessageBinding
 
 class MessageActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityKokoaMessageBinding
+    private val viewModel by viewModels<MessageViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityKokoaMessageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
 }
