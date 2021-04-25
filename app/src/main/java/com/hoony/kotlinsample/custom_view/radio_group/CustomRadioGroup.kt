c7a8ee4014c045a11b4c9d227fab5d6cc0c6530a
thkim9373 <thkim9373@gmail.com>
2021-04-25 21:57:25 +0900

    라디오 그룹 수정

--- app/src/main/java/com/hoony/kotlinsample/custom_view/radio_group/CustomRadioGroup.kt
+++ app/src/main/java/com/hoony/kotlinsample/custom_view/radio_group/CustomRadioGroup.kt
@@ -2,15 +2,66 @@ package com.hoony.kotlinsample.custom_view.radio_group
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
 import android.widget.LinearLayout
+import androidx.annotation.IdRes
+import androidx.core.view.ViewCompat
 
 class CustomRadioGroup @JvmOverloads constructor(
     context: Context,
-    attributeSet: AttributeSet? = null,
+    attrs: AttributeSet? = null,
     defStyleRes: Int = 0
-) : LinearLayout(context, attributeSet, defStyleRes) {
+) : LinearLayout(context, attrs, defStyleRes) {
+
+    private val passThroughHierarchyChangeListener: PassThroughHierarchyChangeListener
 
     init {
-        orientation = VERTICAL
+        passThroughHierarchyChangeListener = PassThroughHierarchyChangeListener()
+        super.setOnHierarchyChangeListener(passThroughHierarchyChangeListener)
+    }
+
+    interface OnCheckedChangeListener {
+        fun onChecked(@IdRes id: Int)
+    }
+
+    private val listener: OnCheckedChangeListener? = null
+
+    /**
+     *ㅈ
+     * A pass-through listener acts upon the events and dispatches them
+     * to another listener. This allows the table layout to set its own internal
+     * hierarchy change listener without preventing the user to setup his.
+     */
+    private inner class PassThroughHierarchyChangeListener : OnHierarchyChangeListener {
+        private val onHierarchyChangeListener: OnHierarchyChangeListener? = null
+
+        /**
+         * {@inheritDoc}
+         */
+        override fun onChildViewAdded(parent: View, child: View) {
+            if (parent === this@CustomRadioGroup && child is CustomRadioButton) {
+                var id = child.getId()
+                // generates an id if it's missing
+                if (id == NO_ID) {
+                    id = ViewCompat.generateViewId()
+                    child.setId(id)
+                }
+                child.listener =
+                    CustomRadioButton.OnCheckChangedListener { isChecked, radioButtonId ->
+                        if (isChecked) listener?.onChecked(radioButtonId)
+                    }
+            }
+            onHierarchyChangeListener?.onChildViewAdded(parent, child)
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        override fun onChildViewRemoved(parent: View, child: View) {
+            if (parent === this@CustomRadioGroup && child is CustomRadioButton) {
+                child.listener = null
+            }
+            onHierarchyChangeListener?.onChildViewRemoved(parent, child)
+        }
     }
 }
